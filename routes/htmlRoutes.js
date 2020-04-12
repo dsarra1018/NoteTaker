f2c2ebd8b5548c65adb0032f5af1af610b88163b
Darren Sarra <adriansarra18@gmail.com>
2020-04-12 00:41:07 -0400

    Set up the htmlRoutes to /notes.

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,8 +2,7 @@ let path = require("path");
 let router = require("express").Router();
 
 router.get("/notes", function(request, response) {
-    // send notes file
-    // 15-HotRestaurant/Solved for more info
+    response.sendFile(path.join(__dirname, "../public/notes.html"))
 });
 
 module.exports = router;
