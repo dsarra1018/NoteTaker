6431ddbddc9c656170e5be085953e3487374666d
Darren Sarra <adriansarra18@gmail.com>
2020-04-11 23:19:29 -0400

    Initialized server.js.

---
+++ server.js
@@ -0,0 +1,15 @@
+// basic server template
+// see 14-FinalStarWarsApp/server-template.js
+// plus require routes
+
+// PLUS the following
+app.use(express.static("public"));
+/*
+^^ creates a path to static front-end files: html, css & js.
+notice how files are referenced in index.html:
+                        <script src="/js/index.js"></scripts>.
+this is because we have created a static path that routes
+to the /public directory automatically.
+*/
+app.use("/api", apiRoutes); // /api/notes
+app.use("/", htmlRoutes); // /notes or /
