87b6ab36236639e9cf47512793462387f3e28ff2
Darren Sarra <adriansarra18@gmail.com>
2020-04-12 19:55:00 -0400

    Created routes for * that would default to index.html when no matching routes is found.

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -5,4 +5,8 @@ router.get("/notes", function(request, response) {
     response.sendFile(path.join(__dirname, "../public/notes.html"))
 });
 
+router.get("*", function(request, response) {
+    response.sendFile(path.join(__dirname, "../public/index.html"));
+});
+
 module.exports = router;
