cbb552fbf774187449e731b3e30b0c5abe034cba
Darren Sarra <adriansarra18@gmail.com>
2020-04-12 20:00:02 -0400

    Added a functionality that would delete notes.

--- js/store.js
+++ js/store.js
@@ -17,3 +17,11 @@ const uuidv1 = require('uuid/v1');
    }
 
    */
+
+module.exports = (body) => {
+    return data = {
+        title: body.title,
+        text: body.text,
+        id: uuidv1()
+    }
+}
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,23 +3,23 @@ const db = require("../db/db.json");
 const store = require("../js/store");
 const path = require("path");
 
+
+// Get notes
 router.get("/notes", function(request, response) {
     response.send(db);
 })
 
+// Post notes where information
 router.post("/notes", function(request, response) {
     db.push(store(request.body));
     response.json(true);
 })
 
-router.delete("/notes/:id", function(request, response) {
-
-})
-
-router.get("*", function(request, response) {
-    response.sendFile(path.join(__dirname, "../public/index.html"));
-})
-
+// Deleting notes
 // /api/notes/:id
+router.delete("note/:id", function(request, response) {
+    db = db.filter(elem => elem.id != request.params.id);
+    response.json(true);
+})
 
 module.exports = router;
